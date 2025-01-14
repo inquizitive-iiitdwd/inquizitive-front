ac1bd42aedb50bcb026bd772263eb55ba0050c85
InQuizitive <inquizitive@iiitdwd.ac.in>
2025-01-14 22:27:17 +0530

    Update eventRegistration.js

--- src/component/eventRegistration.js
+++ src/component/eventRegistration.js
@@ -85,7 +85,7 @@ const EventRegistration = () => {
       };
 
       console.log(data);
-      const response = await axios.post("https://quiz-t7o5.onrender.com/events/eventRegistration", { data }, { withCredentials: true });
+      const response = await axios.post("https://inquizitive-web.onrender.com/events/eventRegistration", { data }, { withCredentials: true });
       console.log(response);
 
       if (response.data.ok) {
