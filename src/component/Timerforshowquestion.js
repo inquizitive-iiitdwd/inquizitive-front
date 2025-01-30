1b7beb3202e3db9b2c85fe6df0e70fd7e993ec1d
InQuizitive <inquizitive@iiitdwd.ac.in>
2025-01-30 19:16:29 +0530

    Update Timerforshowquestion.js

--- src/component/Timerforshowquestion.js
+++ src/component/Timerforshowquestion.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-const Timerforshowquestion = () => {
+const Timerforshowquestion = ({ onTimerEnd }) => {
   const [time, setTime] = useState(100);
 
    useEffect(() => {
