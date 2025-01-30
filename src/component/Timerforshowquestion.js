b997a1ee25ac9d98f4825d2c2083367aa631e5e9
Shanmukha Sai <149197141+Shanmukh1609@users.noreply.github.com>
2025-01-30 19:01:01 +0530

    Update Timerforshowquestion.js
    Automatic submission- onTimerEnd()

--- src/component/Timerforshowquestion.js
+++ src/component/Timerforshowquestion.js
@@ -4,17 +4,20 @@ import axios from 'axios';
 const Timerforshowquestion = () => {
   const [time, setTime] = useState(100);
 
-  useEffect(() => {
+   useEffect(() => {
     async function fetchTimer() {
       try {
         const response = await axios.get("http://localhost:5000/QuizSetUp/getSaveTimer");
         console.log("response", response.data[0].duration);
-        setTime(response.data[0]); // Set the first timer data
+        setTime(response.data[0].duration); // Set the timer duration
       } catch (error) {
         console.error("Error fetching timer:", error);
       }
     }
     fetchTimer();
+  }, []);
+
+  useEffect(() => {
     let timer;
     if (time > 0) {
       timer = setInterval(() => {
@@ -22,9 +25,11 @@ const Timerforshowquestion = () => {
       }, 1000);
     } else if (time === 0) {
       clearInterval(timer);
+      onTimerEnd(); // Call the onTimerEnd function when the timer reaches zero
     }
     return () => clearInterval(timer);
-  }, [time]);
+  }, [time, onTimerEnd]);
+
 
   const formatTime = (seconds) => {
     const hrs = Math.floor(seconds / 3600);
