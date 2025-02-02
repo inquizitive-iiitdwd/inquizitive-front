6c56bf2235c5da5703da7c3ed4eb36c7b6195cf1
InQuizitive <inquizitive@iiitdwd.ac.in>
2025-02-02 15:43:08 +0530

    Update showMarks.js
    Added Timestamp in the showmarks.js

--- src/component/showMarks.js
+++ src/component/showMarks.js
@@ -41,6 +41,7 @@ const ShowMarks = () => {
           {marks.map((mark, index) => (
             <div key={index} className="bg-white shadow-md rounded-lg p-4">
               <p className="font-semibold">Team Name: <span className="font-normal">{mark.teamName}</span></p>
+              <p className="font-semibold">Timestamp: <span className="font-normal">{mark.timestamp}</span></p>
               <p className="font-semibold">Lead Mail ID: <span className="font-normal">{mark.leadmailid}</span></p>
               <p className="font-semibold">Marks: <span className="font-normal">{mark.marks}</span></p>
             </div>
